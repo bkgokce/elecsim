4b862d854d7867e4d8913fef4e9396e79175d9fe
bkgokce <51532846+bkgokce@users.noreply.github.com>
2022-05-26 23:59:00 +0300

    initial test commit

---
+++ test_docker_run_burak.py
@@ -0,0 +1,10 @@
+from elecsim.model.world import World 
+
+number_of_years = 20
+number_of_time_steps = 8
+
+if __name__ == "__main__":
+    world = World(2018, log_level="info", market_time_splices=8, number_of_steps=number_of_years*number_of_time_steps)
+    for years in range(number_of_years):
+        for time_steps in range(number_of_time_steps):
+            world.step()
